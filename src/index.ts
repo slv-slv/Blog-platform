927ca2af89ef1df7d826c95e76c06cc2d5d9bad9
Viacheslav Solovev <slvsl.spb@gmail.com>
2024-12-19 18:25:48 +0300

    implemented the deletion of all data for testing

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { SETTINGS } from './settings.js';
 import { blogsRouter } from './blogs/blogs-router.js';
 import { postsRouter } from './posts/posts-router.js';
+import { db } from './db/db.js';
 
 const app = express();
 
@@ -11,6 +12,12 @@ app.use('/posts', postsRouter);
 
 app.get('/', (req, res) => {
   res.status(200).json({ version: '1.0' });
+});
+
+app.delete('/testing/all-data', (req, res) => {
+  db.blogs = [];
+  db.posts = [];
+  res.status(204).end();
 });
 
 app.listen(SETTINGS.PORT, () => {
