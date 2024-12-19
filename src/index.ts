62c07ecbc885397fd885ad7eaaf9b0d34563521c
Viacheslav Solovev <slvsl.spb@gmail.com>
2024-12-19 19:44:29 +0300

    fixed deleting all data

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import { SETTINGS } from './settings.js';
 import { blogsRouter } from './blogs/blogs-router.js';
 import { postsRouter } from './posts/posts-router.js';
-import { db } from './db/db.js';
+import { deleteAllBlogsDb } from './data-access/blogs-db-access.js';
+import { deleteAllPostsDb } from './data-access/posts-db-access.js';
 
 const app = express();
 
@@ -12,6 +14,12 @@ app.use('/posts', postsRouter);
 
 app.get('/', (req, res) => {
   res.status(200).json({ version: '1.0' });
+});
+
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+  deleteAllBlogsDb();
+  deleteAllPostsDb();
+  res.status(204).json({ message: 'All data has been deleted' });
 });
 
 app.listen(SETTINGS.PORT, () => {
