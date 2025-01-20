d917dca3ec293e992fb0b2b82d178847d2d1739e
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-01-21 02:20:01 +0300

    added result object

---
+++ src/common/types/result-object.ts
@@ -0,0 +1,13 @@
+import { RESULT_STATUS } from './result-status-codes.js';
+
+type ExtensionType = {
+  field: string | null;
+  message: string;
+};
+
+export type Result<T = null> = {
+  status: RESULT_STATUS;
+  errorMessage?: string;
+  extensions: ExtensionType[];
+  data: T;
+};
---
+++ src/common/types/result-status-codes.ts
@@ -0,0 +1,24 @@
+import { HTTP_STATUS } from './http-status-codes.js';
+
+export enum RESULT_STATUS {
+  SUCCESS = 'SUCCESS',
+  NOT_FOUND = 'NOT_FOUND',
+  FORBIDDEN = 'FORBIDDEN',
+  UNAUTHORIZED = 'UNAUTHORIZED',
+  BAD_REQUEST = 'BAD_REQUEST',
+}
+
+export const httpCodeByResult = (status: RESULT_STATUS): HTTP_STATUS => {
+  switch (status) {
+    case RESULT_STATUS.SUCCESS:
+      return HTTP_STATUS.OK_200;
+    case RESULT_STATUS.NOT_FOUND:
+      return HTTP_STATUS.NOT_FOUND_404;
+    case RESULT_STATUS.FORBIDDEN:
+      return HTTP_STATUS.FORBIDDEN_403;
+    case RESULT_STATUS.UNAUTHORIZED:
+      return HTTP_STATUS.UNAUTHORIZED_401;
+    case RESULT_STATUS.BAD_REQUEST:
+      return HTTP_STATUS.BAD_REQUEST_400;
+  }
+};
