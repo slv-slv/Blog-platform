d3e91e0c02a6079904acc59f43ad8cfb26356773
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-01-29 16:33:49 +0300

    deleted commented code

--- src/infrastructure/db/db.ts
+++ src/infrastructure/db/db.ts
@@ -4,14 +4,3 @@ import { MongoCluster } from './mongo-cluster.js';
 export const mongoCluster = new MongoCluster(SETTINGS.MONGO_URL);
 export const mongoClient = mongoCluster.getClient();
 export const db = mongoCluster.getDb(SETTINGS.DB_NAME);
-
-// export const clearDatabase = async (client: MongoClient) => {
-//   if (client) {
-//     const db = client.db();
-//     const collections = await db.collections();
-
-//     for (const collection of collections) {
-//       await collection.deleteMany({});
-//     }
-//   }
-// };
