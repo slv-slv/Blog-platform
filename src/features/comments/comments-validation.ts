416d359102feb17fb39ae7d5e3f4bb8033db2922
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-02-10 17:47:45 +0300

    fixed error message

--- src/features/comments/comments-validation.ts
+++ src/features/comments/comments-validation.ts
@@ -8,7 +8,7 @@ export const commentsValidator = {
     .withMessage('Content must be a string')
     .trim()
     .notEmpty()
-    .withMessage('Blog ID must not be empty')
+    .withMessage('Content must not be empty')
     .isLength({ min: 20, max: 300 })
     .withMessage('Content length must be between 20 and 300 characters'),
 };
