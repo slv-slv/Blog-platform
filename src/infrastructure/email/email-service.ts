7552a8f96a67a729c42c7a92897309c711cdd76a
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-02-12 17:36:11 +0300

    added recovery code sending

--- src/infrastructure/email/email-service.ts
+++ src/infrastructure/email/email-service.ts
@@ -1,9 +1,9 @@
 import { emailTransport } from './nodemailer-transport.js';
 
 class EmailService {
-  async sendConfirmation(to: string, code: string): Promise<void> {
+  async sendConfirmationCode(to: string, code: string): Promise<void> {
     try {
-      const url = new URL('/registration/confirmation', 'https://nightingale.com/');
+      const url = new URL('/auth/registration-confirmation', 'https://nightingale.com/');
       url.searchParams.set('code', code);
 
       const info = await emailTransport.sendMail({
@@ -12,6 +12,25 @@ class EmailService {
         subject: 'Confirm your email',
         text: `Thank you for your registration. To confirm your profile please follow the link below:\n${url.href}`,
         html: `<h1>Thank you for your registration</h1><p>To confirm your profile please follow the link below:</p><a href='${url.href}'>Complete registration</a>`,
+      });
+
+      console.log(info);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async sendRecoveryCode(to: string, code: string): Promise<void> {
+    try {
+      const url = new URL('/auth/password-recovery', 'https://nightingale.com/');
+      url.searchParams.set('code', code);
+
+      const info = await emailTransport.sendMail({
+        from: '"Vyacheslav Solovev" <slvsl.spb@gmail.com>',
+        to: to,
+        subject: 'Password recovery',
+        text: `To reset your password please follow the link below:\n${url.href}`,
+        html: `<h1>Password recovery</h1><p>To reset your password please follow the link below:</p><a href='${url.href}'>Reset password</a>`,
       });
 
       console.log(info);
