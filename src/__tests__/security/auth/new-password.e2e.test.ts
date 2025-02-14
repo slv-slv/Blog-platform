0dd7c6be9e928d19d77d35549df3ab8cdf5ddf5f
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-02-14 03:04:23 +0300

    new tests

---
+++ src/__tests__/security/auth/new-password.e2e.test.ts
@@ -0,0 +1,85 @@
+import { beforeAll, afterAll, describe, it, expect } from 'vitest';
+import request from 'supertest';
+import { dbName, mongoCluster } from '../../../infrastructure/db/db.js';
+import { app } from '../../../app.js';
+import { HTTP_STATUS } from '../../../common/types/http-status-codes.js';
+import { usersService } from '../../../instances/services.js';
+import { CONFIRMATION_STATUS } from '../../../features/users/users-types.js';
+import { usersRepo } from '../../../instances/repositories.js';
+import { usersColl } from '../../../infrastructure/db/collections.js';
+
+beforeAll(async () => {
+  await mongoCluster.run();
+  await mongoCluster.dropDb(dbName);
+});
+
+afterAll(async () => {
+  await mongoCluster.stop();
+  // await mongoMemoryServer.stop();
+});
+
+describe('PASSWORD RECOVERY', () => {
+  const recoveryCode = crypto.randomUUID();
+
+  const pastDate = new Date();
+  pastDate.setHours(pastDate.getHours() - 1);
+
+  const futureDate = new Date();
+  futureDate.setHours(futureDate.getHours() + 1);
+
+  const login = 'NewUser';
+  const email = 'some.email@gmail.com';
+  const hash = 'somehash';
+  const createdAt = new Date().toISOString();
+  const confirmation = {
+    status: CONFIRMATION_STATUS.NOT_CONFIRMED,
+    code: null,
+    expiration: null,
+  };
+  const passwordRecovery = {
+    code: recoveryCode,
+    expiration: futureDate.toISOString(),
+  };
+
+  it('should return 204 if correct recovery code is sent', async () => {
+    await usersRepo.createUser(login, email, hash, createdAt, confirmation, passwordRecovery);
+
+    const body = {
+      newPassword: 'newpassword',
+      recoveryCode,
+    };
+
+    await request(app).post('/auth/new-password').send(body).expect(HTTP_STATUS.NO_CONTENT_204);
+  });
+
+  it('should return 400 if password has incorrect value', async () => {
+    const body = {
+      newPassword: 'pass',
+      recoveryCode,
+    };
+
+    await request(app).post('/auth/new-password').send(body).expect(HTTP_STATUS.BAD_REQUEST_400);
+  });
+
+  it('should return 400 if recovery code is incorrect', async () => {
+    const incorrectCode = crypto.randomUUID();
+
+    const body = {
+      newPassword: 'newpassword',
+      recoveryCode: incorrectCode,
+    };
+
+    await request(app).post('/auth/new-password').send(body).expect(HTTP_STATUS.BAD_REQUEST_400);
+  });
+
+  it('should return 400 if recovery code is expired', async () => {
+    await usersColl.updateOne({ login }, { $set: { 'passwordRecovery.expiration': pastDate.toISOString() } });
+
+    const body = {
+      newPassword: 'newpassword',
+      recoveryCode,
+    };
+
+    await request(app).post('/auth/new-password').send(body).expect(HTTP_STATUS.BAD_REQUEST_400);
+  });
+});
---
+++ src/__tests__/security/auth/password-recovery.e2e.test.ts
@@ -0,0 +1,44 @@
+import { beforeAll, afterAll, describe, it, expect } from 'vitest';
+import request from 'supertest';
+import { dbName, mongoCluster } from '../../../infrastructure/db/db.js';
+import { app } from '../../../app.js';
+import { HTTP_STATUS } from '../../../common/types/http-status-codes.js';
+import { usersService } from '../../../instances/services.js';
+
+beforeAll(async () => {
+  await mongoCluster.run();
+  await mongoCluster.dropDb(dbName);
+});
+
+afterAll(async () => {
+  await mongoCluster.stop();
+  // await mongoMemoryServer.stop();
+});
+
+describe('PASSWORD RECOVERY', () => {
+  const login = 'NewUser';
+  const email = 'example@gmail.com';
+  const password = 'somepassword';
+
+  it('should return 400 if invalid email is sent', async () => {
+    await request(app)
+      .post('/auth/password-recovery')
+      .send({ email: 'invalid^ email@gmail.com' })
+      .expect(HTTP_STATUS.BAD_REQUEST_400);
+
+    await request(app)
+      .post('/auth/password-recovery')
+      .send({ email: '  ' })
+      .expect(HTTP_STATUS.BAD_REQUEST_400);
+  });
+
+  it('should return 204 if not registered email is sent', async () => {
+    await request(app).post('/auth/password-recovery').send({ email }).expect(HTTP_STATUS.NO_CONTENT_204);
+  });
+
+  it('should return 204 if registered email is sent', async () => {
+    await usersService.createUser(login, email, password);
+
+    await request(app).post('/auth/password-recovery').send({ email }).expect(HTTP_STATUS.NO_CONTENT_204);
+  });
+});
