40502e3032de81c1146c30dc5db39ad0d1c9a5f4
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-03-08 01:30:41 +0300

    fixed import

--- src/ioc/container.ts
+++ src/ioc/container.ts
@@ -6,7 +6,7 @@ import { CommentModel } from '../features/comments/comments-model.js';
 import { UserModel } from '../features/users/users-model.js';
 import { SessionModel } from '../security/sessions/sessions-model.js';
 import { RateLimiterModel } from '../security/rate-limiter/rate-limiter-model.js';
-import { CommentLikesModel } from '../features/likes/likes-models.js';
+import { CommentLikesModel } from '../features/likes/comments/comment-likes-models.js';
 
 export const container: Container = new Container({ autoBindInjectable: true, defaultScope: 'Singleton' });
 
