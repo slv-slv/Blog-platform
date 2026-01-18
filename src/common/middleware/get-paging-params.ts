b278806dbc330db29cb2e8f8d4d428f9d19a0d94
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-01-18 18:58:52 +0300

    sortDirection refactoring

--- src/common/middleware/get-paging-params.ts
+++ src/common/middleware/get-paging-params.ts
@@ -4,9 +4,7 @@ import { SETTINGS } from '../../settings.js';
 
 export const getPagingParams = (req: Request, res: Response, next: NextFunction) => {
   const sortBy = req.query.sortBy ?? SETTINGS.PAGING_DEFAULT_PARAMS.sortBy;
-  const sortDirection = req.query.sortDirection
-    ? req.query.sortDirection
-    : SETTINGS.PAGING_DEFAULT_PARAMS.sortDirection;
+  const sortDirection = req.query.sortDirection ?? SETTINGS.PAGING_DEFAULT_PARAMS.sortDirection;
   const pageNumber = req.query.pageNumber ? +req.query.pageNumber : SETTINGS.PAGING_DEFAULT_PARAMS.pageNumber;
   const pageSize = req.query.pageSize ? +req.query.pageSize : SETTINGS.PAGING_DEFAULT_PARAMS.pageSize;
 
